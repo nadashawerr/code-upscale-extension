72219027f3379e8847fcfb36275db0d203567b21
ivansomera <ivansomera@hotmail.com>
2026-03-14 12:26:45 -0700

    refactor command to just show just upscale option

--- src/extension.ts
+++ src/extension.ts
@@ -10,30 +10,18 @@ export function activate(context: vscode.ExtensionContext) {
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "upscale-team" is now active!');
 
-  const ctx = getLineContext();
-  if (!ctx) return;
-  vscode.window.showInformationMessage(ctx.line);
-
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "upscale-team.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from Upscale!");
-    },
-  );
-
   const suggest = vscode.commands.registerCommand(
-    "upscale-team.suggest",
+    "upscale-team.upscale",
     () => {
-      vscode.window.showInformationMessage("This is a test command!");
+      const ctx = getLineContext();
+      if (!ctx) return;
+      vscode.window.showInformationMessage(ctx.line);
     },
   );
 
-  context.subscriptions.push(disposable);
   context.subscriptions.push(suggest);
 }
 
