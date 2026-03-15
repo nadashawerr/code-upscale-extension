0ff836bd46dcb58edbe62105d0677b53eb219cc5
zylliansaab <zylliansaab@gmail.com>
2026-03-15 00:44:32 -0700

    updated refactor to include original code

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { getLineContext } from "./lineContext";
-import { promptRefactor } from './ui';
+import { promptRefactor } from "./ui";
 import { Upscale } from "./llm";
 
 const upscale = new Upscale();
@@ -24,27 +24,13 @@ export function activate(context: vscode.ExtensionContext) {
       if (!ctx) return;
 
       const result = await upscale.refactorCode(ctx.line, ctx.language);
-      vscode.window.showInformationMessage(result);
+	  const original = ctx.line;
+      promptRefactor(result, original);
     },
   );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('upscale-team.helloWorld', () => {
-
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Upscale!');
-		
-	});
-	
-	promptRefactor();
-	
-	context.subscriptions.push(disposable);
-  	context.subscriptions.push(suggest);
+  context.subscriptions.push(suggest);
 }
-
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
